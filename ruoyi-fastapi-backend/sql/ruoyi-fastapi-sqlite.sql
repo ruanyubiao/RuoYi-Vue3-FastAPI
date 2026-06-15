50a3bc615b39351857abff1a648154505fc75661
ryb <ruanyubiao@qq.com>
2026-06-15 08:38:49 +0800

    关闭登录验证码 修改系统名

--- ruoyi-fastapi-backend/sql/ruoyi-fastapi-sqlite.sql
+++ ruoyi-fastapi-backend/sql/ruoyi-fastapi-sqlite.sql
@@ -491,7 +491,7 @@ CREATE TABLE sys_config (
 INSERT INTO sys_config VALUES(1, '主框架页-默认皮肤样式名称',     'sys.index.skinName',               'skin-blue',  'Y', 'admin', datetime('now'), '', NULL, '蓝色 skin-blue、绿色 skin-green、紫色 skin-purple、红色 skin-red、黄色 skin-yellow');
 INSERT INTO sys_config VALUES(2, '用户管理-账号初始密码',         'sys.user.initPassword',            '123456',     'Y', 'admin', datetime('now'), '', NULL, '初始化密码 123456');
 INSERT INTO sys_config VALUES(3, '主框架页-侧边栏主题',           'sys.index.sideTheme',              'theme-dark', 'Y', 'admin', datetime('now'), '', NULL, '深色主题theme-dark，浅色主题theme-light');
-INSERT INTO sys_config VALUES(4, '账号自助-验证码开关',           'sys.account.captchaEnabled',       'true',       'Y', 'admin', datetime('now'), '', NULL, '是否开启验证码功能（true开启，false关闭）');
+INSERT INTO sys_config VALUES(4, '账号自助-验证码开关',           'sys.account.captchaEnabled',       'false',       'Y', 'admin', datetime('now'), '', NULL, '是否开启验证码功能（true开启，false关闭）');
 INSERT INTO sys_config VALUES(5, '账号自助-是否开启用户注册功能', 'sys.account.registerUser',         'false',      'Y', 'admin', datetime('now'), '', NULL, '是否开启注册用户功能（true开启，false关闭）');
 INSERT INTO sys_config VALUES(6, '用户登录-黑名单列表',           'sys.login.blackIPList',            '',           'Y', 'admin', datetime('now'), '', NULL, '设置登录IP黑名单限制，多个匹配项以;分隔，支持匹配（*通配、网段）');
 INSERT INTO sys_config VALUES(7, '用户管理-初始密码修改策略',     'sys.account.initPasswordModify',   '1',          'Y', 'admin', datetime('now'), '', NULL, '0：初始密码修改策略关闭，没有任何提示，1：提醒用户，如果未修改初始密码，则在登录时就会提醒修改密码对话框');
