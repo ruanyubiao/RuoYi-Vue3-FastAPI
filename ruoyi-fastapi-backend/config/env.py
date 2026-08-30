33e2298d7f2cd399b0f6a0fd29029af573357400
ryb <ruanyubiao@qq.com>
2026-08-31 07:43:14 +0800

    token有效期改为10天  14400

--- ruoyi-fastapi-backend/config/env.py
+++ ruoyi-fastapi-backend/config/env.py
@@ -58,8 +58,8 @@ class JwtSettings(BaseSettings):
 
     jwt_secret_key: str = 'b01c66dc2c58dc6a0aabfe2144256be36226de378bf87f72c0c795dda67f4d55'
     jwt_algorithm: str = 'HS256'
-    jwt_expire_minutes: int = 1440
-    jwt_redis_expire_minutes: int = 1440
+    jwt_expire_minutes: int = 14400
+    jwt_redis_expire_minutes: int = 14400
 
 
 class DataBaseSettings(BaseSettings):
